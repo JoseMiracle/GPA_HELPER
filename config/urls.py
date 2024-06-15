1fe9802a9e7ac6e5e820a6de460b116d2c956831
debugtitan <alts.devs@gmail.com>
2024-06-15 11:55:44 +0100

    update urls config

--- config/urls.py
+++ config/urls.py
@@ -3,8 +3,9 @@ URL configuration for gpahelper project.
 
 """
 from django.contrib import admin
+from django.conf import settings
 from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/accounts/', include('gpahelper.accounts.api.urls'), name='accounts'),
+    path(f"api/v{settings.API_VERSION}/accounts/", include('core.accounts.api.urls'), name='accounts'),
 ]
