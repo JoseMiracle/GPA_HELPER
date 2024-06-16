489deb1937d754143017f6a4af5f1543197b4757
debugtitan <alts.devs@gmail.com>
2024-06-16 20:54:24 +0100

    deployment phase 1

--- config/settings/base.py
+++ config/settings/base.py
@@ -2,6 +2,7 @@
 Django settings for gpahelper project.
 
 """
+
 import os
 from pathlib import Path
 from .. import env
@@ -27,56 +28,58 @@ ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOST", default=["*"])
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.sites',
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
+    "django.contrib.sites",
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
     "rest_framework",
     "rest_framework_simplejwt.token_blacklist",
     "corsheaders",
     "drf_spectacular",
     "cloudinary_storage",
     "cloudinary",
+    "whitenoise",
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-ROOT_URLCONF = 'config.urls'
+ROOT_URLCONF = "config.urls"
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
             ],
         },
     },
 ]
 
-WSGI_APPLICATION = 'config.wsgi.application'
+WSGI_APPLICATION = "config.wsgi.application"
 
 
 # Database
 
-if not env.bool("IS_PRODUCTION", False):
+if not env.bool("PRODUCTION", False):
     DATABASES = {
         "default": {
             "ENGINE": "django.db.backends.sqlite3",
@@ -89,12 +92,12 @@ else:
             "ENGINE": env.str(
                 "DJANGO_POSTGRESQL_ENGINE", "django.db.backends.postgresql_psycopg2"
             ),
-            "NAME": env.str("DJANGO_POSTGRESQL_NAME", "***"),
-            "USER": env.str("DJANGO_POSTGRESQL_USER", "***"),
-            "PASSWORD": env.str("DJANGO_POSTGRESQL_PASSWORD", "***"),
-            "HOST": env.str("DJANGO_POSTGRESQL_HOST", "***"),
-            "PORT": env.int("DJANGO_POSTGRESQL_PORT", 5432),
-            "OPTIONS": {"sslmode": "require"},
+            "NAME": env.str("DJANGO_POSTGRES_NAME"),
+            "USER": env.str("DJANGO_POSTGRES_USER"),
+            "PASSWORD": env.str("DJANGO_POSTGRES_PASSWORD"),
+            "HOST": env.str("DJANGO_POSTGRES_HOST", "db"),
+            "PORT": env.int("DJANGO_POSTGRES_PORT", 5432)
+            
         },
     }
 
@@ -104,16 +107,16 @@ else:
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
 
@@ -121,9 +124,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = "UTC"
 
 USE_I18N = True
 
@@ -137,7 +140,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, "static")
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 
 SESSION_ENGINE = "django.contrib.sessions.backends.cached_db"
@@ -145,13 +148,13 @@ SESSION_ENGINE = "django.contrib.sessions.backends.cached_db"
 # ___________ API AND VERSIONING ____________
 API_VERSION = env.str("API_VERSION", default="1")
 
-SITE_ID=1
+SITE_ID = 1
 
 SPECTACULAR_SETTINGS = {
-    'TITLE': 'GPA HELPER',
-    'DESCRIPTION': 'GPA HELPER FOR STUDENT',
-    'VERSION': '1.0.0',
-    'SERVE_INCLUDE_SCHEMA': False,
+    "TITLE": "GPA HELPER",
+    "DESCRIPTION": "GPA HELPER FOR STUDENT",
+    "VERSION": "1.0.0",
+    "SERVE_INCLUDE_SCHEMA": False,
     # OTHER SETTINGS
 }
 
